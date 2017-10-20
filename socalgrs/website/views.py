21da4346fb2b50234657ce6ae5fe3f0b589ba183
David Jacobs <djacobs74@gmail.com>
2017-10-19 18:31:03 -0700

    email, railways

--- socalgrs/website/views.py
+++ socalgrs/website/views.py
@@ -7,6 +7,7 @@ from django.template import RequestContext, Context, loader
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View, DetailView, TemplateView
 from website.models import *
+from django.core.mail import send_mail
 
 class HomeView(View):
 
@@ -40,12 +41,6 @@ class DirectoryView(View):
         template_name = 'directory.html'
         return render(request,template_name, page_data)
 
-class ContactView(View):
-
-    def get(self, request):
-        page_data={}
-        template_name = 'contact.html'
-        return render(request,template_name, page_data)
 
 class MembershipView(View):
 
@@ -72,6 +67,30 @@ class NewslettersView(View):
         template_name = 'newsletters.html'
         return render(request,template_name, page_data)
 
+class ContactView(View):
+
+    def get(self, request):
+        page_data={}
+        template_name = 'contact.html'
+        return render(request,template_name, page_data)
+
+    def post(self, request, *args, **kwargs):
+
+        name=request.POST.get("name", None)
+        email=request.POST.get("email", None)
+        message=request.POST.get("message", None)
+        if email is not None and email != "" and message is not None:
+            
+            tmp_msg = 'Email from: ' + email + ' : ' + name+'\n\n\n'+message
+
+            send_mail("email from SoCalGRS", tmp_msg, email, ["djacobs74@gmail.com"], fail_silently=False)
+            
+            return HttpResponseRedirect("/contact/?success=1")
+
+        else:
+            page_data={'page_title':'contact', 'error':True}
+            template_name = 'contact.html'
+            return render(request,template_name, page_data)
 
 
 
